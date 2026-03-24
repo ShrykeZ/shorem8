2f467f6b9d2a8bd7e4140fc2157e43e31326e5e2
ShrykeZ <9383604+ShrykeZ@users.noreply.github.com>
2026-03-24 21:11:54 +0000

    Update init.sh for better error handling
    Refactor init.sh to improve error handling and readability.

--- init.sh
+++ init.sh
@@ -1,2 +1,9 @@
-#!
-git clone https://${GITHUB_TOKEN}@github.com/ShrykeZ/LWC.git /workspace/myproject && chmod +x /workspace/myproject/* && bash /workspace/myproject/startup.sh
+#!/bin/bash
+set -euo pipefail
+
+cd /workspace
+git clone https://${GITHUB_TOKEN}@github.com/ShrykeZ/LWC.git /workspace/myproject
+
+chmod +x /workspace/myproject/startup.sh
+sed -i 's/\r$//' /workspace/myproject/startup.sh
+bash /workspace/myproject/startup.sh
